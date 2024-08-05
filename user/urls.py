e1d7f078b10373dcc916f3507b12ab1c684623e3
AndrewwSS <clash2clans1one@gmail.com>
2024-08-05 13:05:35 +0300

    add possibility to register and get tokens

--- user/urls.py
+++ user/urls.py
@@ -1,3 +1,13 @@
-urlpatterns = []
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from user.views import CreateUserView
+
+urlpatterns = [
+    path("token", TokenObtainPairView.as_view(), name="token-obtain-pair"),
+    path("token/refresh", TokenRefreshView.as_view(), name="token-refresh"),
+    path("register", CreateUserView.as_view(), name="register"),
+]
 
 app_name = "user"
--- user/views.py
+++ user/views.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from user.serializers import UserCreateSerializer
+
+
+class CreateUserView(CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserCreateSerializer
+    permission_classes = (AllowAny,)
