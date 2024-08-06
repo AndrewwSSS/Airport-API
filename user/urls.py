cdcfcaf0e032aa11a81218afdc8f79fcf2d2b54f
AndrewwSS <clash2clans1one@gmail.com>
2024-08-06 12:29:32 +0300

    update user urls, add slashes to end of endpoints

--- user/urls.py
+++ user/urls.py
@@ -4,9 +4,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from user.views import CreateUserView
 
 urlpatterns = [
-    path("token", TokenObtainPairView.as_view(), name="token-obtain-pair"),
-    path("token/refresh", TokenRefreshView.as_view(), name="token-refresh"),
-    path("register", CreateUserView.as_view(), name="register"),
+    path("token/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("register/", CreateUserView.as_view(), name="register"),
 ]
 
 app_name = "user"
