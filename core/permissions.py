6c770c173027dd41ca90ba2d8b10e319ccf27544
AndrewwSS <clash2clans1one@gmail.com>
2024-08-11 10:59:49 +0300

    permission file reformat

--- core/permissions.py
+++ core/permissions.py
@@ -4,12 +4,6 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 class IsAdminOrAuthenticatedReadOnly(BasePermission):
     def has_permission(self, request, view):
         return bool(
-            (
-                request.user
-                and request.user.is_staff
-            )
-            or (
-                request.user.is_authenticated
-                and request.method in SAFE_METHODS
-            )
+            (request.user and request.user.is_staff)
+            or (request.user.is_authenticated and request.method in SAFE_METHODS)
         )
