3c333cce83232a4ca90a2f396d4dbc3057b2b8da
AndrewwSS <clash2clans1one@gmail.com>
2024-08-11 10:59:27 +0300

    add prefetch crew for FlightViewsSet

--- core/views.py
+++ core/views.py
@@ -177,11 +177,15 @@ class FlightViewSet(GenericMethodsMapping, viewsets.ModelViewSet):
     }
 
     def get_queryset(self):
-        queryset = Flight.objects.select_related(
-            "airplane", "route__source", "route__destination"
-        ).annotate(
-            tickets_available=F("airplane__rows") * F("airplane__seats_in_row")
-            - Count("tickets"),
+        queryset = (
+            Flight.objects.select_related(
+                "airplane", "route__source", "route__destination"
+            )
+            .prefetch_related("crew")
+            .annotate(
+                tickets_available=F("airplane__rows") * F("airplane__seats_in_row")
+                - Count("tickets"),
+            )
         )
         if not self.request.user.is_staff:
             now = timezone.now()
